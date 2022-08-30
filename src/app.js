fbc649fc3c93defab335a30a6b65380bbe893838
hir01 <glazunovden@yandex.ru>
2022-08-30 22:17:33 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,87 +1,101 @@
 /* src/app.js */
 import 'bootstrap'
 import Swiper, { Navigation, Pagination, Grid} from 'swiper';
+import IMask from "imask";
 // Styles
 import 'styles/_app.scss'
 
-new Swiper(".photo__swiper", {
-  modules: [Navigation, Pagination],
-  navigation: {
-    nextEl: ".photo__slider-next",
-    prevEl: ".photo__slider-prev",
-  },
-});
 
-let popularSlider = new Swiper(".popular__slider", {
-  modules: [Grid],
-  slidesPerView: 1,
-  spaceBetween: 30,
-  observer: true,
-  updateOnWindowResize: true,
-  grid: {
-    rows: 3,
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    991: {
-      slidesPerView: 3,
-    },
-  },
-});
-
-new Swiper(".stock__swiper", {
-  modules: [Navigation],
-  slidesPerView: 4,
-  spaceBetween: 30,
-  navigation: {
-    nextEl: ".stock__slider-next",
-    prevEl: ".stock__slider-prev",
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-    },
-    540: {
-      slidesPerView: 2,
-    },
-    991: {
-      slidesPerView: 3,
-    },
-    1024: {
-      slidesPerView: 4,
-    },
-  },
-});
-
-
-ymaps.ready(function () {
-  var myMap = new ymaps.Map('map', {
-      center: [50.599863, 36.573775],
-      zoom: 17,
-      controls: []
-    }, {
-      searchControlProvider: 'yandex#search'
-    }),
-
-    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-    }, {
-      // Опции.
-      // Необходимо указать данный тип макета.
-      iconLayout: 'default#image',
-      // Своё изображение иконки метки.
-      iconImageHref: 'assets/images/logo.svg',
-      // Размеры метки.
-      iconImageSize: [80, 80],
-      // Смещение левого верхнего угла иконки относительно
-      // её "ножки" (точки привязки).
-      iconImageOffset: [-40, -40]
+window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('load', () => {
+    new Swiper(".photo__swiper", {
+      modules: [Navigation, Pagination],
+      navigation: {
+        nextEl: ".photo__slider-next",
+        prevEl: ".photo__slider-prev",
+      },
     });
 
-  myMap.geoObjects
-    .add(myPlacemark)
+    let popularSlider = new Swiper(".popular__slider", {
+      modules: [Grid, Navigation],
+      slidesPerView: 1,
+      spaceBetween: 30,
+      observer: true,
+      updateOnWindowResize: true,
+      grid: {
+        rows: 3,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+      },
+      navigation: {
+        nextEl: ".popular__slider-next",
+        prevEl: ".popular__slider-prev",
+      },
+    });
+
+    new Swiper(".stock__swiper", {
+      modules: [Navigation],
+      slidesPerView: 4,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".stock__slider-next",
+        prevEl: ".stock__slider-prev",
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        540: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+      },
+    });
+
+    ymaps.ready(function () {
+      var myMap = new ymaps.Map('map', {
+          center: [50.599863, 36.573775],
+          zoom: 17,
+          controls: []
+        }, {
+          searchControlProvider: 'yandex#search'
+        }),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: 'assets/images/logo.svg',
+          // Размеры метки.
+          iconImageSize: [80, 80],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-40, -40]
+        });
+
+      myMap.geoObjects
+        .add(myPlacemark)
+    });
+
+    document.querySelectorAll('.js-phone-mask').forEach(item => {
+      IMask(item, {mask: '+{7} (000) 000-00-00', lazy: false,});
+    })
+
+  });
 });
